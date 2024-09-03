3336755b9580568455865e447ac5fe3e42efbd1a
Andres <Andres@Escarabajo>
2024-09-03 18:49:11 -0400

    Commit 2

---
+++ src/main/java/com/mycompany/gestioninventariosupermercado/Compra.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestioninventariosupermercado;
+
+/**
+ *
+ * @author Andres
+ */
+public class Compra {
+    private String id;
+    private Seccion seccionPerteneciente;
+    private Producto productoCompra;
+    private int cantidad;
+
+    public Compra(String id, Seccion seccionPerteneciente, Producto productoCompra, int cantidad) {
+        this.id = id;
+        this.seccionPerteneciente = seccionPerteneciente;
+        this.productoCompra = productoCompra;
+        this.cantidad = cantidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSeccionPerteneciente() {
+        return seccionPerteneciente.getId();
+    }
+
+    public String getProductoVenta() {
+        return productoCompra.getId();
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+}
---
+++ src/main/java/com/mycompany/gestioninventariosupermercado/Proveedor.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestioninventariosupermercado;
+
+/**
+ *
+ * @author Andres
+ */
+public class Proveedor {
+    private String id;
+    private String nombre;
+
+    public Proveedor(String id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
